b7d9549581290809553a8bc64907265443e7bc32
benedgar <bde2wd@virginia.edu>
2015-03-21 13:41:22 -0400

    first commit

---
+++ PointCoin_Marketplace/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for PointCoin_Marketplace project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PointCoin_Marketplace.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
---
+++ pointlist/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    uid = models.OneToOneField(User)
+    date = models.DateField()
+    name = models.CharField(max_length=140)
+    description = models.CharField(max_length=500)
+    public_address = models.CharField(max_length=34)

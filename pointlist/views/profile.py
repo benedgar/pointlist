b5e1fac86a41b06a8c407b393c47e5ae9670a33f
Ben <bde2wd@virginia.edu>
2015-05-03 20:05:23 -0400

    Added ajax functions for spending and adding pointcoin

--- pointlist/views/profile.py
+++ pointlist/views/profile.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from pointlist.pointcoin_tools import check_balance, update_last_balance, spend
+
 __author__ = 'ag'
 
+from pointlist.models import PointcoinAddress
 from django.shortcuts import render
 from django.views.generic import UpdateView
 
@@ -8,3 +13,37 @@ from django.views.generic import UpdateView
 def profileView(UpdateView):
     template = 'pointlist/profile.html'
 
+
+
+@login_required
+def parse_ajax(request):
+    """
+    Parse the ajax request for spending pointcoin
+    """
+    return parse_ajax_helper(request.GET, request.user)
+
+
+def parse_ajax_helper(get, user):
+    try:
+        if 'spend' in get and 'amount' in get and 'toAddress' in get:
+            addr = PointcoinAddress.objects.get(uid=user)
+            flag = spend(get['amount'], get['toAddress'], addr.address)
+            if flag:
+                return HttpResponse("SPENT")
+            else:
+                return HttpResponse("ERROR_SPENDING")
+        elif 'store' in get:
+            addr = PointcoinAddress.objects.get(uid=user)
+            curr_bal = check_balance(addr.address)
+            if curr_bal > addr.last_balance:
+                addr.current_amount = curr_bal - addr.last_balance
+                addr.last_balance = curr_bal
+                addr.save()
+                return HttpResponse("CHANGED")
+            else:
+                return HttpResponse("NOT_CHANGED")
+        else:
+            return HttpResponse("BAD")
+    except Exception as e:
+        return HttpResponse("BAD %s" % e)
+    return HttpResponse("OK")
